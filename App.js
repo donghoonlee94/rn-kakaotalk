cd391033c7d56bb30a68d63676f460165fc4bcf8
이동훈 <donghoon@wemakeprice.com>
2023-05-02 15:16:57 +0900

    add: FriendList & FriendSection components

--- App.js
+++ App.js
@@ -1,23 +1,46 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image } from 'react-native';
-import { getBottomSpace, getStatusBarHeight } from 'react-native-iphone-x-helper';
-import { myProfile } from './src/data';
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
+import { friendProfiles, myProfile } from './src/data';
+import Division from './src/Division';
+import FriendList from './src/FriendList';
+import FriendSection from './src/FriendSection';
 import Header from './src/Header';
 import Margin from './src/Margin';
-import Profile from './src/MyProfile';
+import Profile from './src/Profile';
 
 const statusBarHeight = getStatusBarHeight(true);
-const bottomSpace = getBottomSpace();
 
 export default function App() {
+  const onPressArrow = () => {
+    console.log('1234');
+  };
+
   return (
     <View style={styles.container}>
       <Header/>
+
       <Margin height={10} />
+
       <Profile 
         uri={myProfile.uri}
         name={myProfile.name}
         introduction={myProfile.introduction}
+      />
+
+      <Margin height={15} />
+
+      <Division />
+
+      <Margin height={12} />
+
+      <FriendSection 
+        friendProfileLen={friendProfiles.length}
+        onPressArrow={onPressArrow}
+      />
+
+      <FriendList 
+        data={friendProfiles}
       />
     </View>
   );
@@ -28,6 +51,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     paddingTop: statusBarHeight,
-    paddingBottom: bottomSpace,
+    paddingHorizontal: 15
   },
 });
---
+++ src/FriendList.js
@@ -0,0 +1,24 @@
+import { View } from "react-native";
+import { ScrollView } from "react-native";
+import { getBottomSpace } from "react-native-iphone-x-helper";
+import Margin from "./Margin";
+import Profile from "./Profile";
+
+const bottomSpace = getBottomSpace();
+
+export default (props) => {
+  return (
+    <ScrollView showsVerticalScrollIndicator={false} contentContainerStyle={{ paddingBottom: bottomSpace }}>
+      {props.data.map((item, idx) => (
+        <View key={idx}>
+          <Profile
+            uri={item.uri}
+            name={item.name}
+            introduction={item.introduction}
+          />
+          <Margin height={13} />
+        </View>
+      ))}
+    </ScrollView>
+  );
+};
