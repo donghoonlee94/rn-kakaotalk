b807157b8fe26a8d3d4d3ef82da3546dc497191d
이동훈 <donghoon@wemakeprice.com>
2023-05-02 11:48:47 +0900

    add: react-native-iphone-x-helper

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { getBottomSpace, getStatusBarHeight } from 'react-native-iphone-x-helper';
 import Header from './src/Header';
+
+const statusBarHeight = getStatusBarHeight(true);
+const bottomSpace = getBottomSpace();
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <Header/>
-    </View>
+    </SafeAreaView>
   );
 }
 
@@ -14,7 +18,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
