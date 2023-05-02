09e9bf17d37a5722b64edc0836bc717b62581bd2
이동훈 <donghoon@wemakeprice.com>
2023-05-02 15:49:07 +0900

    add: TabBar component

--- src/FriendList.js
+++ src/FriendList.js
@@ -1,14 +1,11 @@
 import { View } from "react-native";
 import { ScrollView } from "react-native";
-import { getBottomSpace } from "react-native-iphone-x-helper";
 import Margin from "./Margin";
 import Profile from "./Profile";
 
-const bottomSpace = getBottomSpace();
-
 export default (props) => {
   return props.isOpened && (
-    <ScrollView showsVerticalScrollIndicator={false} contentContainerStyle={{ paddingBottom: bottomSpace }}>
+    <ScrollView showsVerticalScrollIndicator={false}>
       {props.data.map((item, idx) => (
         <View key={idx}>
           <Profile
