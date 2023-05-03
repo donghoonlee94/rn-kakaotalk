b5ce17c417ed043335a4554aa16746c16c155de9
이동훈 <94donghoon.lee@gmail.com>
2023-05-03 10:45:14 +0900

    test: commit

--- App.js
+++ App.js
@@ -55,7 +55,7 @@ export default function App() {
     </View>    
   );
 
-  const ListFooterComponent = () => <Margin height={10} />;
+  const ListFooterComponent = () => <Margin height={10} />
 
   return (
     <View style={styles.container}>
